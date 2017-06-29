48552f2695c9ed5182b6965710283231bc7ebc53
gilax <gilad.wolf@mail.huji.ac.il>
2017-06-29 17:15:47 +0300

    change json parse from Moshi to Gson

--- app/src/main/java/ac/huji/agapps/mustsee/mustSeeApi/MovieArray.java
+++
@@ -1,4 +0,0 @@
-package ac.huji.agapps.mustsee.mustSeeApi;
-
-public class MovieArray {
-}
--- app/src/main/java/ac/huji/agapps/mustsee/mustSeeApi/MovieSearch.java
+++ app/src/main/java/ac/huji/agapps/mustsee/mustSeeApi/MovieSearch.java
@@ -1,35 +1,51 @@
 package ac.huji.agapps.mustsee.mustSeeApi;
 
-import android.util.Log;
+import android.support.annotation.Nullable;
 
-import com.squareup.moshi.JsonAdapter;
-import com.squareup.moshi.Moshi;
-
-import java.io.IOException;
+import com.google.gson.Gson;
+import com.google.gson.JsonNull;
 
 public class MovieSearch extends TmdbApiRequest {
 
     private static final String SEARCH_BASE_URL = "/search";
     private static final String QUERY_PARAMETER = "&query=";
+    private static final String PAGE_NUMBER_PARAMETER = "&page=";
 
     private final String TAG = "TMDB Search API";
+    private String query = "";
+    private int nextPageNumber = 1;
+    private int numberOfPages = 1;
 
-    public MovieArray search(String query) {
+    public MovieSearchResults search(String query) {
+        this.query = query;
+        return search(1);
+    }
+
+    public boolean haveNext() {
+        return (this.nextPageNumber <= this.numberOfPages);
+    }
+
+    public MovieSearchResults searchNext() {
+        return search(this.nextPageNumber);
+    }
+
+    @Nullable
+    private MovieSearchResults search(int pageNumber) {
         this.url += SEARCH_BASE_URL + getApiKeyForURL();
         this.url += QUERY_PARAMETER + query;
+        this.url += PAGE_NUMBER_PARAMETER + pageNumber;
 
         String response = getResponseForHTTPGetRequest();
-        Moshi moshi = new Moshi.Builder().build();
-        JsonAdapter<MovieArray> jsonAdapter = moshi.adapter(MovieArray.class);
+        Gson gson = new Gson();
+        MovieSearchResults searchResults = gson.fromJson(response, MovieSearchResults.class);
 
-        MovieArray movieArray = null;
-        try {
-            movieArray = jsonAdapter.fromJson(response);
-        } catch (IOException e) {
-            Log.e(TAG, Log.getStackTraceString(e));
+        if (!searchResults.equals(JsonNull.INSTANCE)) {
+            this.numberOfPages = searchResults.getTotalPages().intValue();
+            this.nextPageNumber = searchResults.getPage().intValue() + 1;
+            return searchResults;
+        } else {
+            return null;
         }
-
-        return movieArray;
     }
 
 }
